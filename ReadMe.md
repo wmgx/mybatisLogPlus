3135b89c40bed4cde22dfccebe3c54ee468dd07b
gx <1109270538@qq.com>
2021-02-14 01:49:55 +0800

    初始化

---
+++ ReadMe.md
@@ -0,0 +1,29 @@
+# 基本描述
+MyBatis的日志类的一个实现，增加了基础的参数替换功能。
+# 效果
+![效果展示](images/show.jpg)
+# 注意
+1. 因为是基础的替换，依靠”？“,”(“与”）“定位，当参数值中含有这三个字符的时候，可能引起替换失败的问题
+2. 输出的级别为INFO级别
+
+# 使用
+## 方法一
+1. 将target目录下的jar包放到项目目录下
+2. 在pom文件中引入
+~~~xml
+    <dependency>
+            <groupId>top.wmgx</groupId>
+            <artifactId>MybatisPlusLogPlus</artifactId>
+            <version>1.0</version>
+            <scope>system</scope>
+            <systemPath>${project.basedir}/{存放的项目下的目录}/MybatisPlusLogPlus-1.0.jar</systemPath>
+        </dependency>
+~~~
+3. 配置文件
+~~~properties
+mybatis-plus.configuration.log-impl=top.wmgx.MybatisPlusLogPlus
+~~~
+## 方法二
+将MybatisPlusLogPlus.java直接拷贝到项目中，配置文件中写这个类的路径即可
+代码很简单，自行修改即可。
+SqlFormatter为Hutool的工具类，可以不用，目的是输出美观的sql。
---
+++ src/main/java/top/wmgx/MybatisPlusLogPlus.java
@@ -0,0 +1,75 @@
+package top.wmgx;
+
+/**
+ * @author wmgx
+ * @create 2021-02-14-0:47
+ **/
+
+import cn.hutool.db.sql.SqlFormatter;
+import org.apache.ibatis.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author wmgx
+ * @create 2021-02-13-23:06
+ **/
+public class MybatisPlusLogPlus implements Log {
+    Logger logger = LoggerFactory.getLogger(Logger.class);
+    String sql = "";
+    public MybatisPlusLogPlus(String s) {
+    }
+    @Override
+    public boolean isDebugEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+        return true;
+    }
+
+    @Override
+    public void error(String s, Throwable throwable) {
+        logger.error(s,throwable);
+    }
+
+    @Override
+    public void error(String s) {
+        logger.error(s);
+    }
+
+    /**
+     * debug 级别信息太多，Info级别信息差不多 调用的Info的输出
+     * @param s
+     */
+    @Override
+    public void debug(String s) {
+        if (s.contains("Preparing: ")){
+            sql=s.split("Preparing: ")[1];
+            return;
+        }
+        if (s.contains("Parameters: ")){
+            // ==> Parameters:  固定开头 16位
+            s = s.split("Parameters: ")[1];
+            for (String t : s.split("\\), "))
+                sql =sql.replaceFirst("\\?","'"+t.substring(0,t.indexOf("("))+"'");
+            return;
+        }
+        if (s.contains("Total:")){
+            logger.info("==> SQL statement: \n"+ SqlFormatter.format(sql)+"\n"+s);
+            return;
+        }
+        logger.info(s);
+    }
+
+    @Override
+    public void trace(String s) {
+        logger.trace(s);
+    }
+
+    @Override
+    public void warn(String s) {
+        logger.warn(s);
+    }
+}
